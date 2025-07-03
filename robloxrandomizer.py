f41bc791813e54dd720b014677d76e2a294ef46c
Noob-97 <noob97xd@gmail.com>
2025-07-03 11:31:38 +0200

    Update robloxrandomizer.py

--- robloxrandomizer.py
+++ robloxrandomizer.py
@@ -13,7 +13,7 @@ currentID = 0
 minVisits = 1000
 
 if "YOUR_API_KEY" in api_key or "" in api_key:
-    print('⚠️ WARNING: variable "api_key" has not been configured.\nRetrieving games' essential information requires an API key for HTTP requests. Before executing any of the scripts you'll need to add your own key to the "api_key" string variable inside. You can create an API key on: https://create.roblox.com/dashboard/credentials?activeTab=ApiKeysTab')
+    print('⚠️ WARNING: variable "api_key" has not been configured.\nRetrieving games essential information requires an API key for HTTP requests. Before executing any of the scripts you will need to add your own key to the "api_key" string variable inside. You can create an API key on: https://create.roblox.com/dashboard/credentials?activeTab=ApiKeysTab')
 
 def randomID():
     universeID = []
